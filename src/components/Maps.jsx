3d202ea574db75441eaab351926996bd7d9d66bf
Issoufi A <issoufi.adam1@gmail.com>
2020-04-28 14:12:16 +0200

    update Maps components

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,11 +1,10 @@
 // Dependencies
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 // import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 // const position = [51.505, -0.09]
-const map = () => {
-  return <p>maps</p>;
-}
+const map = ({ results }) => {
   // <Map center={position} zoom={13}>
   //   <TileLayer
   //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -15,5 +14,14 @@ const map = () => {
   //     <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
   //   </Marker>
   // </Map>
+
+  return <p>maps</p>;
+}
+
+map.prototype = {
+  results: PropTypes.arrayOf(
+    PropTypes.shape({})
+  ).isRequired,
+};
 
 export default map;
